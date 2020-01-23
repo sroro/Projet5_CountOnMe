176b809fa9c8ad6264e42026c3b91435e73a946c
sroro <rodolphe.schnetzer@gmail.com>
2020-01-24 00:01:32 +0100

    refactor notification

--- View/ViewController.swift
+++ View/ViewController.swift
@@ -21,9 +21,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateText), name: Notification.Name("updateCalcul"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(displayError), name: Notification.Name("error"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(displayError2), name: Notification.Name("error2"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(displayError3), name: Notification.Name("error3"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(displayError(_:)), name: Notification.Name("error"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(displayError(_:)), name: Notification.Name("error2"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(displayError(_:)), name: Notification.Name("error3"), object: nil)
         // Do any additional setup after loading the view.
     }
     
@@ -55,9 +55,9 @@ class ViewController: UIViewController {
         textView.text = calcul.calculString
     }
     
-    @objc func displayError(){
+  /*  @objc func displayError(){
         let messsage = "Un operateur est déja mis !"
-        let alertVC = UIAlertController(title: "Zéro!", message: messsage, preferredStyle: .alert)
+        let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
                   alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                   self.present(alertVC, animated: true, completion: nil)
     }
@@ -71,7 +71,28 @@ class ViewController: UIViewController {
            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
            return self.present(alertVC, animated: true, completion: nil)
+       }  */
+    
+    // same alerte with title and message as parameter
+       func alert(_ message: String) {
+           let alertVC = UIAlertController(title: "Erreur", message: message, preferredStyle: .alert)
+           alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+           present(alertVC, animated: true, completion: nil)
        }
+    
+    @objc func displayError(_ notif:Notification)  {
+        let row = notif.name.rawValue
+        switch row {
+        case "error3":
+            alert("Commencez un nouveau calcul")
+        case "error2":
+            alert("Entrez une expression correcte")
+        case "error":
+            alert("Un operateur est déja mis !")
+        default:
+            break
+        }
+    }
     
 
 }
